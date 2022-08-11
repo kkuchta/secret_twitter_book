4f9f56df7cdb2132fcf301a2f97dfa03ddabdd17
Kevin Kuchta <kevin@kevinkuchta.com>
2022-08-10 21:02:57 -0700

    Css fixes and first cover page css

--- cover_page_html.rb
+++ cover_page_html.rb
@@ -14,7 +14,28 @@ def render_to_html(erb_filename, book_index, data)
   puts "  rendering #{erb_filename}"
   out_filename = erb_filename.sub('.html.erb', "_book_#{book_index}.html")
   erb = ERB.new(File.read(erb_filename))
-  result = erb.result_with_hash(data.merge(book_index: book_index))
+
+  # Too lazy for a proper converter
+  book_roman_numeral =
+  case book_index
+    when 0
+      'i'
+    when 1
+      'ii'
+    when 2
+      'iii'
+    when 3
+      'iv'
+    when 4
+      'v'
+    when 5
+      'vi'
+    end
+
+  result = erb.result_with_hash(data.merge(
+    book_index: book_index,
+    book_roman_numeral: book_roman_numeral
+  ))
   File.write('./out/' + out_filename, result)
 end
 
