cfd1188aa0f769d573c3135964e1311492447822
Kevin Kuchta <kevin@kevinkuchta.com>
2022-08-14 16:49:13 -0700

    Redo image layout and update some covers

--- build_books.rb
+++ build_books.rb
@@ -23,37 +23,18 @@ year_months = tweets.group_by do |tweet|
 end.to_a.sort_by(&:first).map(&:second)
 
 books = year_months
-# years = year_months.group_by do |tweet_month|
-#   tweet_month.first[:created_at].year
-# end.select {|year, year_group| year > 2012}
-
-# books = []
-# years.each do |year, year_group|
-#   i = 
-#     case year
-#       when 2016..2018 then 0
-#       when 2019 then 1
-#       when 2020 then 2
-#       when 2021 then 3
-#     end
-#   books[i] ||= []
-#   books[i] += year_group
-
-#   puts "Creating book for year #{year} with #{year_group.flatten.count} tweets, goes in book #{i}"
-# end
-
-# books.each_with_index do |month_group, i|
-#   puts "Book #{i}:"
-#   month_group.each do |month|
-#     puts "  #{month.first[:created_at].strftime('%y %m')}"
-#   end
-# end
 
 books.each_with_index do |month_group, i|
   first_time = month_group.first[:created_at]
   time_code = first_time.strftime('%y_%m')
   puts "Book #{i} for year #{first_time.year}, month #{first_time.month} has #{month_group.count} tweets."
+  tweets_with_images, tweets_without_images = month_group.partition { |tweet| tweet[:media].count > 0 }
   erb = ERB.new(File.read('book_template.html.erb'))
-  result = erb.result_with_hash({year_months: [month_group]})
+  result = erb.result_with_hash({
+    tweets_without_images: tweets_without_images,
+    tweets_with_images: tweets_with_images,
+    tweets: month_group,
+    rand: Random.new(first_time.to_i)
+  })
   File.write("./out/book_#{time_code}.html", result)
 end
